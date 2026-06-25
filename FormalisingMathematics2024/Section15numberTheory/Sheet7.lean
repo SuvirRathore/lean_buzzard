e2ee4318cdf24eabe68adec133df6665d6e13485
Suvir <suvirrathore@gmail.com>
2026-06-25 16:38:24 +0100

    started sol

--- FormalisingMathematics2024/Section15numberTheory/Sheet7.lean
+++ FormalisingMathematics2024/Section15numberTheory/Sheet7.lean
@@ -20,6 +20,24 @@ open scoped BigOperators
 
 open Finset
 
-example (n : ℕ) : ∑ i in range n, i ^ 3 ∣ 3 * ∑ i in range n, i ^ 5 := sorry
+example (n : ℕ) : ∑ i in range n, i ^ 3 ∣ 3 * ∑ i in range n, i ^ 5 := by
+  have h: (3 : ℤ) * ∑ i in range n, (i:ℤ)^5 = (∑ i in range n, (i:ℤ)^3) * (2*n^2 - 2*n - 1) := by
+    induction n with
+    | zero =>
+      dsimp
+      decide
+    | succ d ih =>
+
+      rw[Finset.sum_range_succ, mul_add, ih, Nat.succ_eq_add_one]
+      push_cast
+      have hh: 2 * (d + 1) ^ 2 - 2 * (d + 1) - 1 = 2 * d ^ 2 + 2 * d - 1 := by
+        ring_nf
+
+
+
+
+
+
+
 
 end Section15Sheet7
